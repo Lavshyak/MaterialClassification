9b3958e065321bffed11f119ea8ce2b309a798bb
Lavshyak <lavshyak@gmail.com>
2025-04-05 19:37:42 +0500

    Initial 2

--- MaterialClassification.DataModels/ImageDataAndImagePrediction.cs
+++ MaterialClassification.DataModels/ImageDataAndImagePrediction.cs
@@ -1,4 +1,4 @@
-﻿namespace MaterialClassification;
+﻿namespace MaterialClassification.DataModels;
 
 public class ImageData
 {
--- MaterialClassification.WebApi/MaterialClassificationService.cs
+++ MaterialClassification.WebApi/MaterialClassificationService.cs
@@ -1,6 +1,55 @@
-﻿namespace MaterialClassification.WebApi;
+﻿using System.Text;
+using MaterialClassification.DataModels;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 
-public class MaterialClassificationService
+namespace MaterialClassification.WebApi;
+
+public class MaterialClassificationService : IDisposable
 {
+    private readonly PredictionEngine<ImageData, ImagePrediction> _predictionEngine;
+    private readonly DataViewSchema _outputSchema;
     
+    public MaterialClassificationService(IConfiguration configuration)
+    {
+        var modelPath = configuration.GetValue<string>("ModelPath") ?? throw new InvalidOperationException();
+        var mlContext = new MLContext();
+        ITransformer trainedModel = mlContext.Model.Load(modelPath, out var inputSchema);
+        Console.WriteLine("Модель загружена.");
+        _predictionEngine = mlContext.Model.CreatePredictionEngine
+            <ImageData, ImagePrediction>
+            (trainedModel);
+        Console.WriteLine("PredictionEngine создан.");
+        _outputSchema = trainedModel.GetOutputSchema(inputSchema) ?? throw new InvalidOperationException();
+    }
+
+    public string Predict(string imagePath)
+    {
+        var imageData = new ImageData { ImagePath = imagePath, };
+        var sb = new StringBuilder();
+        var prediction = _predictionEngine.Predict(imageData);
+        sb.AppendLine(prediction.PredictedLabelValue);
+        var labelKeyColumn = _outputSchema["LabelKey"];
+        VBuffer<ReadOnlyMemory<char>> keyValues = default;
+        labelKeyColumn.GetKeyValues(ref keyValues);
+        var items = keyValues.Items().ToArray();
+        if (items.Length != prediction.Score.Length)
+        {
+            throw new InvalidOperationException();
+        }
+        var classesScores = items.Zip(prediction.Score,
+                (keyValuePair, predictionScore) => new
+                    { ClassName = new string(keyValuePair.Value.Span), PredictionScore = predictionScore })
+            .ToArray();
+        sb.AppendLine("Вероятности принадлежности к классам материалов:");
+        sb.AppendLine(string.Join(", ",
+            classesScores.OrderByDescending(cs => cs.PredictionScore).Take(5)
+                .Select(cs => $"{cs.ClassName}: {cs.PredictionScore}")));
+        return sb.ToString();
+    }
+
+    public void Dispose()
+    {
+        _predictionEngine.Dispose();
+    }
 }
